472941bc7f07f7799113f92da2f2490d088e7224
samqu <samquiroz385@gmail.com>
2023-11-25 15:49:33 -0300

    Uso de base de datos e implementación del registro

--- app/src/main/java/com/example/mirefri/CrearCuentaActivity.kt
+++ app/src/main/java/com/example/mirefri/CrearCuentaActivity.kt
@@ -1,9 +1,11 @@
 package com.example.mirefri
 
+import MyToolbar
 import Usuario
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -20,14 +22,14 @@ class CrearCuentaActivity : AppCompatActivity() {
         createbtn.setOnClickListener{
 
             val nombre = findViewById<EditText>(R.id.NombreCA_IF).text.toString()
-            val apellido = findViewById<EditText>(R.id.ApellidoCA_IF).text.toString()
+            //val apellido = findViewById<EditText>(R.id.ApellidoCA_IF).text.toString()
             val correo = findViewById<EditText>(R.id.CorreoAC_IF).text.toString()
             val clave = findViewById<EditText>(R.id.ClaveAC_IF).text.toString()
             val repetirClave = findViewById<EditText>(R.id.RepetirClaveAC_IF).text.toString()
 
             if (clave == repetirClave) {
 
-                val usuario = Usuario(nombre, apellido, correo, clave, 1, mutableListOf(Productos()))
+                val usuario = Usuario(nombre, "", correo, clave, 1, mutableListOf(Productos()))
                 val resultIntent = Intent()
                 resultIntent.putExtra("Usuario", usuario)
                 setResult(RESULT_OK, resultIntent)
@@ -36,5 +38,18 @@ class CrearCuentaActivity : AppCompatActivity() {
                 Toast.makeText(this, "Las claves no coinciden", Toast.LENGTH_LONG).show()
             }
         }
+        MyToolbar().Show(this,this.getString(R.string.LICrear))
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if(item.itemId == android.R.id.home){
+
+
+            val resultIntent = Intent()
+            setResult(RESULT_CANCELED, resultIntent)
+            finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
